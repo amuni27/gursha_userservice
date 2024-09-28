217938b29c65f3904cd8f6f770c34b488cd00adf
amuni27 <amanuuka@gm1il.com>
2024-09-28 16:43:50 -0500

    first commit

--- model/User.ts
+++ model/User.ts
@@ -4,6 +4,7 @@ import {Schema,model} from 'mongoose';
 const UserSchema = new Schema({
     first_name: {type: String, required: true},
     last_name: {type: String, required: true},
+    phone_number: {type: String, required: true},
     email: {type: String, required: true},
     password:{type: String, required: true},
 })
